1f3938009eb2f0407e10c42b051feeec2a247a31
Robert Nelson <robertcnelson@gmail.com>
2011-12-08 13:44:20 -0600

    qemu: regression testing
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- qemu/build_qemu.sh
+++ qemu/build_qemu.sh
@@ -20,11 +20,67 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+unset DISABLE_WERROR
+unset SUB_VER
+unset SUB_DATE
+
 DIR=$PWD
 TEMPDIR=$(mktemp -d)
 
 LINARO_VER="2011.11"
 QEMU_VER="0.15.91"
+
+#looking for: vexpress-a9
+#LINARO_VER="2011.10"
+#QEMU_VER="0.15.50"
+#DISABLE_WERROR="--disable-werror"
+#export CC=gcc-4.5
+
+#looking for: vexpress-a9
+#LINARO_VER="2011.09"
+#QEMU_VER="0.15.50"
+#DISABLE_WERROR="--disable-werror"
+#export CC=gcc-4.5
+
+#looking for: vexpress-a9
+#LINARO_VER="2011.08"
+#QEMU_VER="0.15.50"
+#DISABLE_WERROR="--disable-werror"
+#export CC=gcc-4.5
+
+#looking for: vexpress-a9
+#LINARO_VER="2011.07"
+#SUB_VER="-0"
+#QEMU_VER="0.14.50"
+#DISABLE_WERROR="--disable-werror"
+#export CC=gcc-4.5
+
+#looking for: vexpress-a9
+#LINARO_VER="2011.06"
+#SUB_VER="-0"
+#QEMU_VER="0.14.50"
+#DISABLE_WERROR="--disable-werror"
+#export CC=gcc-4.5
+
+#LINARO_VER="2011.04"
+#SUB_VER="-1"
+#SUB_DATE="${SUB_VER}"
+#QEMU_VER="0.14.50"
+#DISABLE_WERROR="--disable-werror"
+#export CC=gcc-4.5
+
+#LINARO_VER="2011.04"
+#SUB_VER="-0"
+#QEMU_VER="0.14.50"
+#DISABLE_WERROR="--disable-werror"
+#export CC=gcc-4.5
+
+#LINARO_VER="2011.03"
+#SUB_VER="-1"
+#SUB_DATE="${SUB_VER}"
+#QEMU_VER="0.14.50"
+#DISABLE_WERROR="--disable-werror"
+#export CC=gcc-4.5
 
 mkdir -p ${DIR}/dl/
 
@@ -34,24 +90,24 @@ echo "ubuntu/debian: sudo apt-get install libglib2.0-dev"
 echo ""
 
 function dl_qemu {
- wget -c --directory-prefix=${DIR}/dl/ http://launchpad.net/qemu-linaro/trunk/${LINARO_VER}/+download/qemu-linaro-${QEMU_VER}-${LINARO_VER}.tar.gz
+ wget -c --directory-prefix=${DIR}/dl/ http://launchpad.net/qemu-linaro/trunk/${LINARO_VER}${SUB_DATE}/+download/qemu-linaro-${QEMU_VER}-${LINARO_VER}${SUB_VER}.tar.gz
 }
 
 function extract_qemu {
  rm -rf ${DIR}/qemu-build/ || true
  mkdir -p ${DIR}/qemu-build/
- tar xf ${DIR}/dl/qemu-linaro-${QEMU_VER}-${LINARO_VER}.tar.gz -C ${DIR}/qemu-build/
+ tar xf ${DIR}/dl/qemu-linaro-${QEMU_VER}-${LINARO_VER}${SUB_VER}.tar.gz -C ${DIR}/qemu-build/
 }
 
 function build_qemu {
- cd ${DIR}/qemu-build/qemu-linaro-${QEMU_VER}-${LINARO_VER}
- ./configure --prefix=/usr --target-list=arm-softmmu
+ cd ${DIR}/qemu-build/qemu-linaro-${QEMU_VER}-${LINARO_VER}${SUB_VER}
+ ./configure --prefix=/usr --target-list=arm-softmmu ${DISABLE_WERROR}
  make
  cd ${DIR}
 }
 
 function install_qemu {
- cd ${DIR}/qemu-build/qemu-linaro-${QEMU_VER}-${LINARO_VER}
+ cd ${DIR}/qemu-build/qemu-linaro-${QEMU_VER}-${LINARO_VER}${SUB_VER}
  sudo make install
  cd ${DIR}
 }
